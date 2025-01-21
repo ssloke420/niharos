9515b8471c621d75e659c08d3287c26fdaf6bfdb
ssloke420 <131798522+ssloke420@users.noreply.github.com>
2025-01-21 12:40:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+  function updateDateTime() {
+    const timeElement = document.getElementById('time');
+    const now = new Date();
 
+    // Format the time
+    const timeOptions = {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true
+    };
+    const time = now.toLocaleTimeString('en-US', timeOptions);
+
+    // Format the date
+    const dateOptions = {
+        weekday: 'short',
+        month: 'short',
+        day: 'numeric'
+    };
+    let date = now.toLocaleDateString('en-US', dateOptions);
+    date = date.replace(',', ''); // Remove the comma
+
+    timeElement.innerHTML = `${date} &nbsp; &nbsp; ${time}`;
+}
+// Update the time every second
+    setInterval(updateDateTime, 1000);
+
+// Initial call to set the time immediately on page load
+    updateDateTime();
